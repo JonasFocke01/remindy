4d23b1a008563bc0e9b38b4e26535e5e5027edc7
Jonas <jonas-focke@mailbox.org>
2023-02-09 21:26:17 +0100

    feat: 🔥 some improvements

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use colored::*;
 use notify_rust::Notification;
 use soloud::*;
-use std::env;
 use std::io::Write;
+use std::{env, vec};
 
 use chrono::naive::NaiveTime;
 use chrono::offset::Local;
@@ -49,7 +49,7 @@ fn help(origin: String) {
     print!("\n");
     print!(
         "{}\n",
-        " remindy [NAME: string] [DURATION: number][MODIFIER: 'h' | 'm' | 's']".green()
+        " remindy [DURATION: number][MODIFIER: 'h' | 'm' | 's']".green()
     );
     print!(" remindy testmeeting 15m\n");
     print!("\n");
@@ -65,23 +65,32 @@ fn help(origin: String) {
     print!("\n");
     print!(
         "{}\n",
-        " remindy [NAME: string] [TIME: [number][number]:[number][number]]".green()
+        " remindy [TIME: [number][number]:[number][number]]".green()
     );
-    print!(
-        "{}\n",
-        " optional parameter: '-d' to remind in X days".green()
-    );
-    print!(" remindy testmeeting 15:00 -d 1\n");
+    print!(" remindy testmeeting 15:00\n");
     print!("\n");
     print!(
         "{}\n",
         " This will spawn a new countdown which will notify you tomorrow at 15:00 O'Clock."
             .bright_green()
     );
+    print!(
+        "{}\n",
+        "====================== Options ===================\n".purple()
+    );
+    print!("-n: Give your timer a name ( -n testmeeting )\n\n");
+    print!("-p: Display a progressbar, no parameter needed\n\n");
+    print!("-a: Call Alexa when timer ends, no parameter needed\n\n");
+    print!("-d: Push timer X days into the future ( -d 1 )\n\n");
     std::process::exit(0);
 }
 
-fn build_notification(message: String, timer_length_in_ms: &mut u64) {
+fn build_notification(
+    message: String,
+    timer_length_in_ms: &mut u64,
+    trigger_alexa: bool,
+    display_progress_bar: bool,
+) {
     let initial_timer_length_in_ms = timer_length_in_ms.clone();
     let mut interval_timestamp = time::Instant::now();
 
@@ -91,31 +100,33 @@ fn build_notification(message: String, timer_length_in_ms: &mut u64) {
     );
 
     // call iobroker
-    let mut easy = Easy::new();
-    easy.url(
-        format!(
-            "{}/set/0_userdata.0.endpoints.reminderMessage?value={}&prettyPrint",
-            IOBROKER_IP, message
+    if trigger_alexa {
+        let mut easy = Easy::new();
+        easy.url(
+            format!(
+                "{}/set/0_userdata.0.endpoints.reminderMessage?value={}&prettyPrint",
+                IOBROKER_IP, message
+            )
+            .as_str(),
         )
-        .as_str(),
-    )
-    .unwrap();
-    easy.get(true).unwrap();
-    let transfer = easy.transfer();
-    transfer.perform().unwrap();
+        .unwrap();
+        easy.get(true).unwrap();
+        let transfer = easy.transfer();
+        transfer.perform().unwrap();
 
-    let mut easy = Easy::new();
-    easy.url(
-        format!(
-            "{}/set/0_userdata.0.endpoints.nextReminder?value={}&prettyPrint",
-            IOBROKER_IP, timer_length_in_ms
+        let mut easy = Easy::new();
+        easy.url(
+            format!(
+                "{}/set/0_userdata.0.endpoints.nextReminder?value={}&prettyPrint",
+                IOBROKER_IP, timer_length_in_ms
+            )
+            .as_str(),
         )
-        .as_str(),
-    )
-    .unwrap();
-    easy.get(true).unwrap();
-    let transfer = easy.transfer();
-    transfer.perform().unwrap();
+        .unwrap();
+        easy.get(true).unwrap();
+        let transfer = easy.transfer();
+        transfer.perform().unwrap();
+    }
 
     print!(
         "\n {} {} {} {}:{}:{}\n",
@@ -145,15 +156,21 @@ fn build_notification(message: String, timer_length_in_ms: &mut u64) {
             ]
             .join("");
 
-            print!(
-                "\r {} {}:{}:{} [{}>{}]",
+            let result_string = format!(
+                "\r {} {}:{}:{}",
                 "Time left:".bright_green(),
                 format!("{:02}", *timer_length_in_ms / 3_600_000).bright_red(),
                 format!("{:02}", (*timer_length_in_ms / 60_000) % 60).bright_red(),
                 format!("{:02}", (*timer_length_in_ms / 1000) % 60).bright_red(),
-                progress_bar_filler,
-                progress_bar_space
             );
+            if display_progress_bar {
+                print!(
+                    "{} [{}>{}]",
+                    result_string, progress_bar_filler, progress_bar_space
+                );
+            } else {
+                print!("{}", result_string);
+            }
             *timer_length_in_ms -= 1000;
             std::io::stdout().flush().unwrap_or_default();
             std::thread::sleep(std::time::Duration::from_millis(
@@ -167,10 +184,12 @@ fn build_notification(message: String, timer_length_in_ms: &mut u64) {
         "====================Time is up!====================".bright_red()
     );
     std::io::stdout().flush().unwrap_or_default();
-    Notification::new()
-        .summary(message.as_str())
-        .show()
-        .unwrap();
+    if message.len() != 0 {
+        Notification::new()
+            .summary(message.as_str())
+            .show()
+            .unwrap();
+    }
 
     let sl = Soloud::default().unwrap();
     let mut wav = audio::Wav::default();
@@ -182,31 +201,40 @@ fn build_notification(message: String, timer_length_in_ms: &mut u64) {
 }
 
 fn parse_time_remaining(options: &mut Vec<String>) -> Result<String, String> {
-    if options.len() != 2 && (options.len() != 4) {
-        return Err(format!(
-            "Wrong parameters: {:?} expected testmeeting 15:00 -OR- testmeeting 15:00 -d 1",
-            options
-        ));
-    }
-    let mut timer_days_in_ms: u64 = 0;
-    if options.len() == 4 {
-        if options[2] == "-d" {
-            let timer_days_as_bytes = options.pop().unwrap().into_bytes();
-            for i in 0..(timer_days_as_bytes.len()) {
-                timer_days_in_ms += (timer_days_as_bytes[i] as u64 - 48) * 86_400_000;
+    let mut timer_input_string = options.remove(0);
+
+    let mut timer_length_in_ms: u64 = 0;
+    let mut trigger_alexa = false;
+    let mut timer_name: String = String::new();
+    let mut display_progress_bar = false;
+    while options.len() > 0 {
+        match options.remove(0).as_str() {
+            "-p" => display_progress_bar = true,
+            "-a" => trigger_alexa = true,
+            "-d" => {
+                if options.len() > 0 {
+                    timer_length_in_ms +=
+                        (options.remove(0).parse::<u64>().unwrap_or(48) - 48) * 86_400_000
+                } else {
+                    return Err(format!("-d needs a parameter!"));
+                }
             }
-            options.pop().unwrap();
-        } else {
-            return Err(format!("Wrong parameter: {}, expected '-d'", options[2]));
+            "-n" => {
+                if options.len() > 0 {
+                    timer_name = options.remove(0)
+                } else {
+                    return Err(format!("-n needs a parameter!"));
+                }
+            }
+            e => return Err(format!("unexpected parameter {} found", e)),
         }
     }
-
-    let mut timer_input_string = options.remove(1);
+    if trigger_alexa && timer_name.len() == 0 {
+        return Err("-a needs to be used in combination with -n [NAME]".to_string());
+    }
 
     match timer_input_string.find(":") {
         Some(2) => {
-            let mut timer_length_in_ms: u64 = 0;
-
             let timer_input_bytes = timer_input_string.into_bytes();
 
             timer_length_in_ms += (timer_input_bytes[0] as u64 - 48) * 36_000_000;
@@ -216,17 +244,19 @@ fn parse_time_remaining(options: &mut Vec<String>) -> Result<String, String> {
 
             let timestamp_target =
                 NaiveTime::from_num_seconds_from_midnight_opt(timer_length_in_ms as u32 / 1000, 0)
-                    .unwrap();
+                    .expect("error while creating timestamp_target");
             let system_time_now = Local::now().time();
-            let timestamps_difference =
+            let mut timestamps_difference =
                 (timestamp_target - system_time_now).num_milliseconds() as u64;
             if timestamps_difference >= 86_400_000 {
                 return Err("the desired time sould be in the future".to_string());
             }
 
             build_notification(
-                options.pop().unwrap(),
-                &mut (timestamps_difference + timer_days_in_ms),
+                timer_name,
+                &mut (timestamps_difference),
+                trigger_alexa,
+                display_progress_bar,
             );
         }
         Some(_) => {
@@ -267,7 +297,12 @@ fn parse_time_remaining(options: &mut Vec<String>) -> Result<String, String> {
                 e => return Err(format!("modifier unknown: {}", e)),
             };
 
-            build_notification(options.pop().unwrap(), &mut timer_length_in_ms);
+            build_notification(
+                timer_name,
+                &mut timer_length_in_ms,
+                trigger_alexa,
+                display_progress_bar,
+            );
         }
     }
 
