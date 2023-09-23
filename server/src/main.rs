4ed7e8739c8d86e4e45ac2d09d27c84666533f9e
jonas <@DISTRIB_ID=Pop.com>
2023-09-23 20:21:46 +0200

    Implement axum routes

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,10 @@
+
+use axum::{
+    extract::{rejection::JsonRejection, State},
+    http::StatusCode,
+    routing::{get, post},
+    Json, Router,
+};
 use serde::{Deserialize, Serialize};
 
 use time::{Duration, OffsetDateTime, UtcOffset};
@@ -19,7 +26,50 @@ async fn main() {
     }]));
 
     // axum
+    let reminders_axum_clone = Arc::clone(&reminders);
+    tokio::spawn(async move {
+        let app = Router::new()
+            .route("/reminder", get(all_reminder))
+            .route("/reminder", post(add_reminder))
+            .with_state(reminders_axum_clone);
+
+        axum::Server::bind(&SocketAddr::new(
+            IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
+            4321,
+        ))
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+    });
 
     // terminal display
+async fn all_reminder(
+    State(reminders): State<Arc<Mutex<Vec<Reminder>>>>,
+) -> (StatusCode, Json<Vec<Reminder>>) {
+    if let Ok(reminders) = reminders.lock() {
+        (StatusCode::OK, Json(reminders.clone()))
+    } else {
+        (StatusCode::INTERNAL_SERVER_ERROR, Json(vec![]))
+    }
 }
 
+#[derive(Clone, Deserialize)]
+struct ApiReminder {
+    name: String,
+    finish_time: OffsetDateTime,
+}
+async fn add_reminder(
+    State(reminders): State<Arc<Mutex<Vec<Reminder>>>>,
+    new_reminder: Result<Json<ApiReminder>, JsonRejection>,
+) -> StatusCode {
+    if let Ok(mut reminders) = reminders.lock() {
+        if let Ok(Json(new_reminder)) = new_reminder {
+            reminders.push(new_reminder.into());
+            StatusCode::OK
+        } else {
+            StatusCode::UNPROCESSABLE_ENTITY
+        }
+    } else {
+        StatusCode::INTERNAL_SERVER_ERROR
+    }
+}
