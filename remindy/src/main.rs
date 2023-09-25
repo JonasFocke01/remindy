e090907d1779da3d6ff108c339dc564697612e0b
jonas <@DISTRIB_ID=Pop.com>
2023-09-25 18:26:48 +0200

    Rework keyboard commands

--- remindy/src/main.rs
+++ remindy/src/main.rs
@@ -34,7 +34,7 @@ use duration_string::DurationString;
 use notify_rust::Notification;
 use serde::{Deserialize, Serialize};
 
-use time::{Duration, OffsetDateTime, UtcOffset};
+use time::{format_description, Duration, OffsetDateTime, Time, UtcOffset};
 
 #[derive(Clone, Serialize, Deserialize)]
 enum ReminderType {
@@ -233,21 +233,21 @@ async fn main() {
         .unwrap();
         stdout.write_all(b"Remindy started\n\r").unwrap();
         stdout.write_all(b"===============\n\r").unwrap();
+        stdout.write_all(b"'j', 'k' -> up, down\n\r").unwrap();
+        stdout.write_all(b"'n' -> new\n\r").unwrap();
+        stdout.write_all(b"'rn' -> rename\n\r").unwrap();
+        stdout.write_all(b"'rt' -> retime\n\r").unwrap();
+        stdout.write_all(b"'s' -> snooze\n\r").unwrap();
         stdout
-            .write_all(b"'CTRL' + 'c' ends the programm gracefully\n\r")
+            .write_all(b"'d' -> delete (double tab needed)\n\r")
             .unwrap();
         stdout
-            .write_all(b"'n' pauses the program and waits for a new reminder from stdin\n\r")
+            .write_all(b"'rs' -> restart (double tab needed)\n\r")
             .unwrap();
-        stdout.write_all(b"Navigate with 'j' and 'k'\n\r").unwrap();
+        stdout.write_all(b"'esc' -> unmark everything\n\r").unwrap();
         stdout
-            .write_all(b"'d' marks as deletable. ('d' on marked reminder deletes it)\n\r")
+            .write_all(b"'CTRL' + 'c' -> exit(0)\n\r")
             .unwrap();
-        stdout
-            .write_all(b"'r' marks as restartable. ('r' on marked reminder restarts it)\n\r")
-            .unwrap();
-        stdout.write_all(b"'esc' to unmark everything\n\r").unwrap();
-        stdout.write_all(b"===============\n\n\r").unwrap();
         if let Ok(mut reminders) = reminders.try_lock() {
             for (i, reminder) in reminders.iter_mut().enumerate() {
                 if i == cursor_position {
@@ -272,7 +272,6 @@ async fn main() {
                                 std::process::exit(0);
                             }
                         }
-
                         KeyCode::Char('n') => {
                             if let Ok(mut reminders) = reminders.lock() {
                                 let now = OffsetDateTime::now_utc()
@@ -283,12 +282,87 @@ async fn main() {
                                     reminder_type: ReminderType::Duration,
                                     duration: Duration::new(0, 0),
                                     finish_time: now,
-                                    finish_notifications_send: false,
+                                    finish_notifications_send: true,
                                     delete_flag: false,
                                     restart_flag: false,
                                 })
                             }
                         }
+                        KeyCode::Char('r') => match read().unwrap() {
+                            Event::Key(event) => match event.code {
+                                KeyCode::Char('s') => {
+                                    if let Ok(mut reminders) = reminders.lock() {
+                                        let reminder = reminders.get_mut(cursor_position).unwrap();
+                                        if reminder.restart_flag {
+                                            reminder.restart()
+                                        } else {
+                                            reminder.restart_flag = true;
+                                        }
+                                    }
+                                }
+                                KeyCode::Char('n') => {
+                                    execute!(
+                                        stdout,
+                                        cursor::Show,
+                                        cursor::MoveTo(0, cursor_position as u16)
+                                    )
+                                    .unwrap();
+                                    let _trash_bin = disable_raw_mode().is_ok();
+                                    let mut name = String::new();
+                                    std::io::stdin().read_line(&mut name).unwrap();
+                                    name = name.replace('\n', "");
+                                    if let Ok(mut reminders) = reminders.lock() {
+                                        let reminder = reminders.get_mut(cursor_position).unwrap();
+                                        reminder.name = name;
+                                    }
+                                    let _trash_bin = enable_raw_mode().is_ok();
+                                }
+                                KeyCode::Char('t') => {
+                                    let _trash_bin = disable_raw_mode().is_ok();
+                                    let mut time_input = String::new();
+                                    std::io::stdin().read_line(&mut time_input).unwrap();
+                                    time_input = time_input.replace('\n', "");
+                                    if let Ok(mut reminders) = reminders.lock() {
+                                        let reminder = reminders.get_mut(cursor_position).unwrap();
+                                        reminder.name = time_input.clone();
+                                    }
+                                    let now = OffsetDateTime::now_utc()
+                                        .to_offset(UtcOffset::from_hms(2, 0, 0).unwrap());
+                                    let mut finish_time = OffsetDateTime::now_utc()
+                                        .to_offset(UtcOffset::from_hms(2, 0, 0).unwrap());
+                                    if let Ok(mut reminders) = reminders.lock() {
+                                        let reminder = reminders.get_mut(cursor_position).unwrap();
+                                        #[allow(clippy::useless_conversion)]
+                                        if time_input.chars().all(|e| e.is_ascii_digit()) {
+                                            finish_time = finish_time.replace_time(
+                                                Time::parse(
+                                                    time_input.as_str(),
+                                                    &format_description::parse("[hour]:[minute]")
+                                                        .unwrap(),
+                                                )
+                                                .unwrap(),
+                                            );
+                                            reminder.reminder_type = ReminderType::Time;
+                                            reminder.start_time = now;
+                                            reminder.finish_time = finish_time;
+                                            reminder.duration = finish_time - now;
+                                        } else {
+                                            let d: core::time::Duration =
+                                                DurationString::from_string(time_input)
+                                                    .unwrap()
+                                                    .into();
+                                            reminder.duration =
+                                                Duration::from(d.try_into().unwrap());
+                                            reminder.finish_time = now + d;
+                                            reminder.reminder_type = ReminderType::Duration;
+                                        }
+                                    }
+                                    let _trash_bin = enable_raw_mode().is_ok();
+                                }
+                                _ => (),
+                            },
+                            _ => (),
+                        },
                         KeyCode::Char('k') => cursor_position = cursor_position.saturating_sub(1),
                         KeyCode::Char('j') => {
                             if let Ok(reminders) = reminders.lock() {
@@ -307,16 +381,6 @@ async fn main() {
                                     }
                                 } else {
                                     reminder.delete_flag = true;
-                                }
-                            }
-                        }
-                        KeyCode::Char('r') => {
-                            if let Ok(mut reminders) = reminders.lock() {
-                                let reminder = reminders.get_mut(cursor_position).unwrap();
-                                if reminder.restart_flag {
-                                    reminder.restart()
-                                } else {
-                                    reminder.restart_flag = true;
                                 }
                             }
                         }
