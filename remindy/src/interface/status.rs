e168df7eab38009d6539b693e0a5b66f9bfcc54a
jonas <@DISTRIB_ID=Pop.com>
2023-10-06 17:34:45 +0200

    Reenable help screen

--- remindy/src/interface/status.rs
+++ remindy/src/interface/status.rs
@@ -5,27 +5,27 @@ use crate::api::ApiStatus;
 pub fn build_status_box(api_status: &Arc<Mutex<ApiStatus>>) -> String {
     let mut result = String::new();
     result.push_str("           =======================================\n\r");
-    // result.push_str(format!("           | {:<36}|\n\r", "'j', 'k' -> up, down").as_str());
-    // result.push_str(format!("           | {:<36}|\n\r", "'n' -> new").as_str());
-    // result.push_str(format!("           | {:<36}|\n\r", "'rn' -> rename").as_str());
-    // result.push_str(format!("           | {:<36}|\n\r", "'rt' -> retime").as_str());
-    // result.push_str(format!("           | {:<36}|\n\r", "'s' -> snooze").as_str());
-    // result.push_str(
-    //     format!(
-    //         "           | {:<36}|\n\r",
-    //         "'d' -> delete (double tab needed)"
-    //     )
-    //     .as_str(),
-    // );
-    // result.push_str(
-    //     format!(
-    //         "           | {:<36}|\n\r",
-    //         "'rs' -> restart (double tab needed)"
-    //     )
-    //     .as_str(),
-    // );
-    // result.push_str(format!("           | {:<36}|\n\r", "'esc' -> unmark everything").as_str());
-    // result.push_str(format!("           | {:<36}|\n\r", "'CTRL' + 'c' -> exit(0)").as_str());
+    result.push_str(format!("           | {:<36}|\n\r", "'j', 'k' -> up, down").as_str());
+    result.push_str(format!("           | {:<36}|\n\r", "'n' -> new").as_str());
+    result.push_str(format!("           | {:<36}|\n\r", "'rn' -> rename").as_str());
+    result.push_str(format!("           | {:<36}|\n\r", "'rt' -> retime").as_str());
+    result.push_str(format!("           | {:<36}|\n\r", "'s' -> snooze").as_str());
+    result.push_str(
+        format!(
+            "           | {:<36}|\n\r",
+            "'d' -> delete (double tab needed)"
+        )
+        .as_str(),
+    );
+    result.push_str(
+        format!(
+            "           | {:<36}|\n\r",
+            "'rs' -> restart (double tab needed)"
+        )
+        .as_str(),
+    );
+    result.push_str(format!("           | {:<36}|\n\r", "'esc' -> unmark everything").as_str());
+    result.push_str(format!("           | {:<36}|\n\r", "'CTRL' + 'c' -> exit(0)").as_str());
     if let Ok(api_status) = api_status.lock() {
         result.push_str(
             format!(
