4b52d6afc466cc2c8b09378e0e14828d526e7aac
jonas <@DISTRIB_ID=Pop.com>
2023-10-07 18:14:09 +0200

    Reminder ended last event how shows time since

--- remindy/src/interface/past_event.rs
+++ remindy/src/interface/past_event.rs
@@ -1,6 +1,7 @@
 use std::fmt::Display;
 
 use colored::Colorize;
+use time::{OffsetDateTime, UtcOffset};
 
 use crate::reminder::Reminder;
 
@@ -16,12 +17,48 @@ pub enum PastEvent {
 impl Display for PastEvent {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            PastEvent::ReminderEnded(reminder) => write!(f, "{} {}", reminder.name().green(), "ended".green()),
+            #[allow(clippy::arithmetic_side_effects)]
+            PastEvent::ReminderEnded(reminder) => {
+                let now = OffsetDateTime::now_utc();
+                if let Ok(offset) = UtcOffset::from_hms(2, 0, 0) {
+                    now.to_offset(offset);
+                }
+                let finished_ago = now - reminder.finish_time();
+                write!(
+                    f,
+                    "{} {} {:0>2}{}{:0>2}{}{:0>2} {}",
+                    reminder.name().green(),
+                    "ended".green(),
+                    (finished_ago.whole_hours() - finished_ago.whole_days() * 24)
+                        .to_string()
+                        .green(),
+                    ":".green(),
+                    (finished_ago.whole_minutes() - finished_ago.whole_hours() * 60)
+                        .to_string()
+                        .green(),
+                    ":".green(),
+                    (finished_ago.whole_seconds() - finished_ago.whole_minutes() * 60)
+                        .to_string()
+                        .green(),
+                    "ago".green()
+                )
+            }
             PastEvent::WrongInput => write!(f, "{}", "Wrong input detected".bright_red()),
-            PastEvent::ReminderCreated(reminder) => write!(f, "{} {}", reminder.name().bright_green(), "created".bright_green()),
-            PastEvent::ReminderEdited(reminder) => write!(f, "{} {}", reminder.name().blue(), "edited".blue()),
-            PastEvent::ReminderDeleted(reminder) => write!(f, "{} {}", reminder.name().red(), "deleted".red()),
-            PastEvent::ReminderSnooze(reminder) => write!(f, "{} {}", reminder.name().blue(), "snoozed".blue()),
+            PastEvent::ReminderCreated(reminder) => write!(
+                f,
+                "{} {}",
+                reminder.name().bright_green(),
+                "created".bright_green()
+            ),
+            PastEvent::ReminderEdited(reminder) => {
+                write!(f, "{} {}", reminder.name().blue(), "edited".blue())
+            }
+            PastEvent::ReminderDeleted(reminder) => {
+                write!(f, "{} {}", reminder.name().red(), "deleted".red())
+            }
+            PastEvent::ReminderSnooze(reminder) => {
+                write!(f, "{} {}", reminder.name().blue(), "snoozed".blue())
+            }
             PastEvent::None => write!(f, "{}", " ".black()),
         }
     }
