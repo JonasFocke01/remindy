2135f458ccbc9b5e22b091e2e6c4ef52b7367913
Jonas Focke <jonas.focke@NB-2021-51.local>
2024-02-05 16:09:57 +0100

    init light frontend

---
+++ simple_js_frontend/src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let reminders = [];
+
+	onMount(async () => {
+		// const response = await fetch('http://jonrrrs.duckdns.org:6969/reminers');
+		const response = await fetch('//188.136.69.200:6969/reminders');
+		reminders = await response.json();
+	});
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+{#each reminders as reminder}
+	<p>
+		{reminder.name}
+	</p>
+{/each}
