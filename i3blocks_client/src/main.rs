6a1b5bfe64fe402b5db923f176e1aa8fe04b228a
jonas <@DISTRIB_ID=Pop.com>
2024-05-02 18:13:18 +0200

    Improve textouput for `i3blocks`

--- i3blocks_client/src/main.rs
+++ i3blocks_client/src/main.rs
@@ -31,13 +31,31 @@ fn main() {
     let mut reminders = reminders.take(7);
 
     print!(
-        "<span color='#50FF30'>{}</span>  <span color='#00BBBB'>{}</span>  <span color='#50FF00'>{}</span>  <span color='#00BBBB'>{}</span>  <span color='#50FF30'>{}</span>  <span color='#00BBBB'>{}</span>  <span color='#50FF30'>{}</span>",
-        reminders.next().expect("Could not read Reminders"),
-        reminders.next().expect("Could not read Reminders"),
-        reminders.next().expect("Could not read Reminders"),
-        reminders.next().expect("Could not read Reminders"),
-        reminders.next().expect("Could not read Reminders"),
-        reminders.next().expect("Could not read Reminders"),
-        reminders.next().expect("Could not read Reminders")
+        "<span color='#50FF30'>{}</span>  <span color='#00BBBB'>{}</span>  <span color='#50FF30'>{}</span>  <span color='#00BBBB'>{}</span>  <span color='#50FF30'>{}</span>  <span color='#00BBBB'>{}</span>  <span color='#50FF30'>{}</span>",
+
+        format_reminder(reminders.next().expect("Could not read Reminders")),
+        format_reminder(reminders.next().expect("Could not read Reminders")),
+        format_reminder(reminders.next().expect("Could not read Reminders")),
+        format_reminder(reminders.next().expect("Could not read Reminders")),
+        format_reminder(reminders.next().expect("Could not read Reminders")),
+        format_reminder(reminders.next().expect("Could not read Reminders")),
+        format_reminder(reminders.next().expect("Could not read Reminders"))
     );
+}
+
+#[allow(clippy::arithmetic_side_effects)]
+fn format_reminder(reminder: &Reminder) -> String {
+    if let Some(time_left) = reminder.remaining_duration() {
+        format!(
+            "{} {:0>2}{}{:0>2}{}{:0>2}",
+            reminder.name(),
+            (time_left.whole_hours() - time_left.whole_days() * 24).to_string(),
+            ":",
+            (time_left.whole_minutes() - time_left.whole_hours() * 60).to_string(),
+            ":",
+            (time_left.whole_seconds() - time_left.whole_minutes() * 60).to_string()
+        )
+    } else {
+        String::new()
+    }
 }
